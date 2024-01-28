49a0e58119688bd615f1c643c396d28785f699df
Danhelo <danielhelopuccini@outlook.com>
2024-01-27 23:23:07 -0500

    Update Topology.js

--- frontend/components/Topology.js
+++ frontend/components/Topology.js
@@ -29,23 +29,32 @@ const Topology = ({ onOptionChange, onUserCountChange }) => {
     background: selectedOption === 'Line' ? '#f0f0f0' : 'transparent',
   };
 
+  const textStyle = {
+    color: '#ec5c3d', // Red color for the text
+  };
+
   return (
     <div className='centered'>
       <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
         <div style={{ ...optionStyle, background: selectedOption === 'Line' ? '#f0f0f0' : 'transparent' }} onClick={() => handleOptionChange('Line')}>
           <FaNetworkWired />
+          <span style={textStyle}>  Line</span> {/* Text description for Line */}
         </div>
         <div style={{ ...optionStyle, background: selectedOption === 'Ring' ? '#f0f0f0' : 'transparent' }} onClick={() => handleOptionChange('Ring')}>
           <FaRing />
+          <span style={textStyle}>  Ring</span> {/* Text description for Ring */}
         </div>
         <div style={{ ...optionStyle, background: selectedOption === 'Star' ? '#f0f0f0' : 'transparent' }} onClick={() => handleOptionChange('Star')}>
           <FaStar />
+          <span style={textStyle}>  Star</span> {/* Text description for Star */}
         </div>
         <div style={{ ...optionStyle, background: selectedOption === 'Mesh' ? '#f0f0f0' : 'transparent' }} onClick={() => handleOptionChange('Mesh')}>
           <FaProjectDiagram />
+          <span style={textStyle}>  Mesh</span> {/* Text description for Mesh */}
         </div>
         <div style={{ ...optionStyle, background: selectedOption === 'Hybrid' ? '#f0f0f0' : 'transparent' }} onClick={() => handleOptionChange('Hybrid')}>
           <FaLifeRing />
+          <span style={textStyle}>  Hybrid</span> {/* Text description for Hybrid */}
         </div>
       </div>
 
